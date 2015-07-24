7592f128bdd0adb79a671ee1812e4d93618f3738
noraescareno <nescareno93@gmail.com>
2015-07-24 12:12:41 -0500

    Issue #23 Controller Users: Agregar Seguridad Nivel Permiso-Rol (Metodo Crear)

--- application/controllers/users.php
+++ application/controllers/users.php
@@ -43,20 +43,20 @@ class Users extends CI_Controller {
   		* 3.- Capturista
   		*/
 	}
-	
+
 	public function index()
 	{ 
 		// Si tienes Rol de SuperAdministrador entras sin permisos
 		if (ROL == SUPERROL) {
 
-			$this->load->model('tbl_user_crud_model'); //mando llamar al model 'tbl_user_crud_model' como un tipo include
-			$data['cargar_users'] = $this->tbl_user_crud_model->cargar_users();  //aqui ejecuto el metodo 'cargar_users' de la clase ''tbla_user_crud_model
-			$data['cargar_users_lista'] = $this->tbl_user_crud_model->cargar_lista();
-			$this->load->model('tbl_roles_model');
 			$data['cargar_roles']= $this->tbl_roles_model->cargar_roles();
 			$data['username'] = USER;
 			$data['rol'] = ROL;
 			$data['get_all'] = $this->permisos_model->get_all();
+			$this->load->model('tbl_user_crud_model'); //mando llamar al model 'tbl_user_crud_model' como un tipo include
+			$data['cargar_users'] = $this->tbl_user_crud_model->cargar_users();  //aqui ejecuto el metodo 'cargar_users' de la clase ''tbla_user_crud_model
+			$data['cargar_users_lista'] = $this->tbl_user_crud_model->cargar_lista();
+			$this->load->model('tbl_roles_model');
 			$this->load->view('header_view');
 			$this->load->view('cabecera_view');
 			$this->load->view('menu_view');
@@ -70,28 +70,28 @@ class Users extends CI_Controller {
 			if ($tiene_permiso == TRUE) {
 				
 				// EL USUARIO SI TIENE ACCESO AL METODO
-				$this->load->model('tbl_user_crud_model'); //mando llamar al model 'tbl_user_crud_model' como un tipo include
-				$data['cargar_users'] = $this->tbl_user_crud_model->cargar_users();  //aqui ejecuto el metodo 'cargar_users' de la clase ''tbla_user_crud_model
-				$data['cargar_users_lista'] = $this->tbl_user_crud_model->cargar_lista();
-				$this->load->model('tbl_roles_model');
 				$data['cargar_roles']= $this->tbl_roles_model->cargar_roles();
 				$data['username'] = USER;
 				$data['rol'] = ROL;
 		 		$data['get_all'] = $this->permisos_model->get_all();
+				$this->load->model('tbl_user_crud_model'); //mando llamar al model 'tbl_user_crud_model' como un tipo include
+				$data['cargar_users'] = $this->tbl_user_crud_model->cargar_users();  //aqui ejecuto el metodo 'cargar_users' de la clase ''tbla_user_crud_model
+				$data['cargar_users_lista'] = $this->tbl_user_crud_model->cargar_lista();
+				$this->load->model('tbl_roles_model');
 				$this->load->view('header_view');
 				$this->load->view('cabecera_view');
 				$this->load->view('menu_view');
 				$this->load->view('contenedor_users_view',$data);
 				$this->load->view('footer_view');
 			}else{
-				$this->load->model('tbl_user_crud_model'); //mando llamar al model 'tbl_user_crud_model' como un tipo include
-				$data['cargar_users'] = $this->tbl_user_crud_model->cargar_users();  //aqui ejecuto el metodo 'cargar_users' de la clase ''tbla_user_crud_model
-				$data['cargar_users_lista'] = $this->tbl_user_crud_model->cargar_lista();
-				$this->load->model('tbl_roles_model');
 				$data['cargar_roles']= $this->tbl_roles_model->cargar_roles();
 				$data['username'] = USER;
 				$data['rol'] = ROL;
 		 		$data['get_all'] = $this->permisos_model->get_all();
+				$this->load->model('tbl_user_crud_model'); //mando llamar al model 'tbl_user_crud_model' como un tipo include
+				$data['cargar_users'] = $this->tbl_user_crud_model->cargar_users();  //aqui ejecuto el metodo 'cargar_users' de la clase ''tbla_user_crud_model
+				$data['cargar_users_lista'] = $this->tbl_user_crud_model->cargar_lista();
+				$this->load->model('tbl_roles_model');
 				$this->load->view('header_view');
 				$this->load->view('cabecera_view');
 				$this->load->view('menu_view');
@@ -103,12 +103,13 @@ class Users extends CI_Controller {
 
 	public function crear()
 	{
-		$ci_session= $this->session->userdata('username');
-		if (empty($ci_session)===TRUE) {
-			redirect(base_url('welcome/logout')); 
-		}
-		else
-		{
+		// Si tienes Rol de SuperAdministrador entras sin permisos
+		if (ROL == SUPERROL) {
+
+			$data['cargar_roles']= $this->tbl_roles_model->cargar_roles();
+			$data['username'] = USER;
+			$data['rol'] = ROL;
+			$data['get_all'] = $this->permisos_model->get_all();
 			$nombre=$_POST['nombre'];
 			$id_tipo=$_POST['id_tipo'];
 			$username=$_POST['username'];
@@ -120,15 +121,49 @@ class Users extends CI_Controller {
 			$this->load->model('tbl_user_crud_model'); 
 			$nuevo = $this->tbl_user_crud_model->agregar_users($nombre, $id_tipo, $username, $password, $email, $tel, $id_status);
 			
-			/*if ($nuevo==TRUE) {
-				$this->users->index();
-			}
-			else {
-				
-				echo "DATOS NO INSERTADOS";
-			}*/
-
+			
 			$this->index();
+		}// Pero si no eres SuperAdministrador, te vamos a verificar tus permisos de acceso al Controler y Metodo
+		else
+		{
+			$metodo = $this->uri->segment(2); // Metodo de la URL
+			$tiene_permiso = $this->permisos_model->verify_metodo(ROL,COMPONENTE,$metodo);
+			if ($tiene_permiso == TRUE) {
+				
+				// EL USUARIO SI TIENE ACCESO AL METODO
+				$data['cargar_roles']= $this->tbl_roles_model->cargar_roles();
+				$data['username'] = USER;
+				$data['rol'] = ROL;
+		 		$data['get_all'] = $this->permisos_model->get_all();
+		 		$nombre=$_POST['nombre'];
+				$id_tipo=$_POST['id_tipo'];
+				$username=$_POST['username'];
+				$password=$_POST['password'];
+				$email=$_POST['email'];
+				$tel=$_POST['tel'];
+				$id_status=$_POST['id_status'];
+
+				$this->load->model('tbl_user_crud_model'); 
+				$nuevo = $this->tbl_user_crud_model->agregar_users($nombre, $id_tipo, $username, $password, $email, $tel, $id_status);
+				
+				
+				$this->index();
+				}else{
+				$data['cargar_roles']= $this->tbl_roles_model->cargar_roles();
+				$data['username'] = USER;
+				$data['rol'] = ROL;
+		 		$data['get_all'] = $this->permisos_model->get_all();
+		 		$nombre=$_POST['nombre'];
+				$id_tipo=$_POST['id_tipo'];
+				$username=$_POST['username'];
+				$password=$_POST['password'];
+				$email=$_POST['email'];
+				$tel=$_POST['tel'];
+				$id_status=$_POST['id_status'];
+
+				$this->load->model('tbl_user_crud_model'); 
+				$this->load->view('sorry_view',$data);
+			}
 		}
 	}
 
