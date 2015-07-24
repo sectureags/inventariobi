9b4ab57f3f9002bea6a9e392841a9338b2bc8809
noraescareno <nescareno93@gmail.com>
2015-07-24 09:55:13 -0500

    Issue #21 Agregar Seguridad Nivel Permisos-Rol Controller Home (Metodo Index)

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -24,21 +24,72 @@ class Home extends CI_Controller {
 		parent::__construct();
 		$this->load->library('session');
 		$this->load->library('encrypt');
-	}
-
-	public function index()
-	{
+		// Si la sesion no tiene datos, redireccionarlo fuera del sistema
 		$ci_session= $this->session->userdata('username');
 		if (empty($ci_session)===TRUE) {
 			redirect(base_url('welcome/logout')); 
 		}
-		else{
+		// Se Definen constantes para facilitar la programacion
+		define("SUPERROL", 1); // "SuperAdministrador"
+		define('ROL',$this->session->userdata('rol'));
+	    define('COMPONENTE',$this->uri->segment(1));
+	    define('USER',$this->session->userdata('username'));
+	    //
+  		$this->load->model('permisos_model');
+ 		$this->load->model('tbl_roles_model');
+  		/*
+  		* Tabla de Roles:
+  		* 1.- Super Administrador
+  		* 2.- Administrador
+  		* 3.- Capturista
+  		*/
+	}
+
+	public function index()
+	{
+		// Si tienes Rol de SuperAdministrador entras sin permisos
+		if (ROL == SUPERROL) {
+
+			$data['cargar_roles'] = $this->tbl_roles_model->cargar_roles();
+			$data['username'] = USER;
+			$data['rol'] = ROL;
+			$data['get_all'] = $this->permisos_model->get_all();
 			$this->load->view('header_view');  ///se manda llamar a la vista///
 			$this->load->view('cabecera_view');
 			$this->load->view('menu_view');
 			$this->load->view('home_view');
-			$this->load->view('footer_view');	
-			}	
+			$this->load->view('footer_view');
+		}// Pero si no eres SuperAdministrador, te vamos a verificar tus permisos de acceso al Controler y Metodo
+		else
+		{
+			$metodo = $this->uri->segment(2); // Metodo de la URL
+			$tiene_permiso = $this->permisos_model->verify_metodo(ROL,COMPONENTE,$metodo);
+			if ($tiene_permiso == TRUE) {
+
+				// EL USUARIO SI TIENE ACCESO AL METODO
+				$data['cargar_roles'] = $this->tbl_roles_model->cargar_roles();
+				$data['username'] = USER;
+				$data['rol'] = ROL;
+		 		$data['get_all'] = $this->permisos_model->get_all();
+				$this->load->view('header_view');  ///se manda llamar a la vista///
+				$this->load->view('cabecera_view');
+				$this->load->view('menu_view');
+				$this->load->view('home_view');
+				$this->load->view('footer_view');
+			}else{
+
+				$data['cargar_roles'] = $this->tbl_roles_model->cargar_roles();
+				$data['username'] = USER;
+				$data['rol'] = ROL;
+				$data['get_all'] = $this->permisos_model->get_all();
+				$this->load->view('header_view');  ///se manda llamar a la vista///
+				$this->load->view('cabecera_view');
+				$this->load->view('menu_view');
+				$this->load->view('sorry_view',$data);
+				$this->load->view('footer_view');
+			}				
+			
+		}
 	}
 
 	public function seguridad()
