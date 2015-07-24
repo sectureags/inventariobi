d40709730b06d6855c8334395da399f99b77a43f
noraescareno <nescareno93@gmail.com>
2015-07-24 10:13:53 -0500

    Issue #21 Agrregar Seguridad Nivel Permiso-Rol Controller Home (Metodo Bienes_Informaticos)

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -141,18 +141,50 @@ class Home extends CI_Controller {
 
 	public function bienes_informaticos()
 	{	
-		$ci_session= $this->session->userdata('username');
-		if (empty($ci_session)===TRUE) {
-			redirect(base_url('welcome/logout')); 
-		}
-		else
-		{
+		// Si tienes Rol de SuperAdministrador entras sin permisos
+		if (ROL == SUPERROL) {
+
+			$data['cargar_roles'] = $this->tbl_roles_model->cargar_roles();
+			$data['username'] = USER;
+			$data['rol'] = ROL;
+			$data['get_all'] = $this->permisos_model->get_all();
 			$this->load->view('header_view');
 			$this->load->view('cabecera_view');
 			$this->load->view('menu_view');
 			$this->load->view('contenedor_bi_view');
 			$this->load->view('footer_view');
+		}// Pero si no eres SuperAdministrador, te vamos a verificar tus permisos de acceso al Controler y Metodo
+		else
+		{
+			$metodo = $this->uri->segment(2); // Metodo de la URL
+			$tiene_permiso = $this->permisos_model->verify_metodo(ROL,COMPONENTE,$metodo);
+			if ($tiene_permiso == TRUE) {
+
+				// EL USUARIO SI TIENE ACCESO AL METODO
+				$data['cargar_roles'] = $this->tbl_roles_model->cargar_roles();
+				$data['username'] = USER;
+				$data['rol'] = ROL;
+		 		$data['get_all'] = $this->permisos_model->get_all();
+		 		$this->load->view('header_view');
+				$this->load->view('cabecera_view');
+				$this->load->view('menu_view');
+				$this->load->view('contenedor_bi_view');
+				$this->load->view('footer_view');
+			}else{
+
+				$data['cargar_roles'] = $this->tbl_roles_model->cargar_roles();
+				$data['username'] = USER;
+				$data['rol'] = ROL;
+				$data['get_all'] = $this->permisos_model->get_all();
+				$this->load->view('header_view');
+				$this->load->view('cabecera_view');
+				$this->load->view('menu_view');
+				$this->load->view('sorry_view',$data);
+				$this->load->view('footer_view');
+			}				
+			
 		}
+		
 	}
 
 	public function reportes()
