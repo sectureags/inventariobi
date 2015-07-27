80bcb0346a8add22a3dab0234bdf2dab45c3efc4
sectureags <sectureags@gmail.com>
2015-07-27 15:47:54 -0500

    se agrega filtro por rol en permisos [iss29]

--- application/views/contenedor_permisos_view.php
+++ application/views/contenedor_permisos_view.php
@@ -1,7 +1,13 @@
 <div id="container">
-	<div class="row well">        
-		User: <?php echo $username; ?><br>
-		Rol: (
+	<nav class="navbar navbar-default">
+	  <div class="container-fluid">
+	    <div class="navbar-header">
+	      
+	    </div>
+	    <div>
+	      <ul class="nav navbar-nav navbar-left">
+	        <li class="active">User: <?php echo $username; ?><br>
+			<small>(
 			<?php
 				foreach ($cargar_roles as $tipo) :
 					if ($rol == $tipo->id_tipo ) {
@@ -10,55 +16,40 @@
 						echo "</strong>";
 					}
 		 		endforeach; ?> 
-			)
-	</div>
-	<div class="row">
-	  <ul class="nav navbar-nav navbar-left">
-        <li>
-        <?php $attributes = array('class' => 'navbar-form navbar-left', 'role' => 'menu');
-            echo form_open(base_url('permisos/create'), $attributes); ?>   
-            <div class="form-group">
-              <label for="name">Rol</label>       
-			  	<select class="form-control" name="rol" id="rol" placeholder="Rol">
-				  	<option>Seleccione un Rol</option>
-					<?php foreach ($cargar_roles as $rol) :?>      
-					 	<option value="<?php echo $rol->id_tipo; ?>"><?php echo $rol->descripcion;?></option>                 
-				 	<?php endforeach; ?>       
-			 	</select>
-
-            </div>
-            <div class="form-group">
-              <?php 
-                echo form_label('Componente: ',  'componente' ) ; 
-                echo form_input('componente', '', 'id="componente" class="form-control" placeholder="Componente"');
-            ?>
-            </div>
-            <div class="form-group">
-              <?php 
-                echo form_label('Recurso: ',  'recurso' ) ; 
-                echo form_input('recurso', '', 'id="recurso" class="form-control" placeholder="Recurso"');
-              ?>
-            </div>
-            <?php echo form_submit('submit',  'Agregar Nuevo', 'class="btn btn-primary"' ); ?>
-          <?php echo form_close(); ?>
-        </li>            
-      </ul>
-	</div>
-		
-	<div class="row">	
-	<table class="table table-bordered">
+			)</small></li>
+	        <li>
+        	<form class="navbar-form navbar-right" role="form" method="post" action="<?php echo base_url('permisos/index');?>">
+			<div class="form-group">       
+			  	<label for="id_tipo">Filtrar por Rol</label> 
+				<select class="form-control" name="id_tipo" id="id_tipo" onchange="this.form.submit()"><!--Cuando detectes un cambio en tus selects traeme los datos de la seleccion-->
+				<option>Roles</option>
+			    <?php foreach ($cargar_roles as $rol) :?>      
+			 	<option value="<?php echo $rol->id_tipo; ?>"><?php echo $rol->descripcion;?></option>                 
+			 	<?php endforeach; ?>       
+				</select>
+			</div>
+			</form>
+        </li>
+	      
+	      </ul>
+	    </div>
+	  </div>
+	</nav>
+	
+	<div class="">	
+	<table class="table table-bordered table-hover table-condensed">
 		<tr>		
 		<th class="text-center">Rol</th>
 		<th class="text-center">Componente</th>
 		<th class="text-center">Recurso</th>
 		<th class="text-center">Permiso</th>
-		<th class="text-center">Accion</th>
+		
 		</tr>
 		<?php
 			if ( (is_array($get_all) == TRUE ) ) {
 				
 				foreach ($get_all as $value) {
-			  		echo "<tr>";		  	  	
+			  		echo "<tr class='warning'>";		  	  	
 		
 			  		echo "<td>";
 		  	  		foreach ($cargar_roles as $rol) :
@@ -73,14 +64,14 @@
 			  		echo "<td>";
 			  	  	echo "$value->recurso";		  	  	
 			  		echo "</td>";
-			  		echo "<td class='text-center'>";
-			  	  	echo "$value->permiso";		  	  	
-			  		echo "</td>";
+			  		
 			  		echo "<td class='text-center'>";
 			  		if ($value->permiso != TRUE) { ?>
-			  			<a href="<?php echo base_url('permisos/activar/'.$value->id) ;?>">Activar</a>
+			  			<a href="<?php echo base_url('permisos/activar/'.$value->id) ;?>"><input type="checkbox"></a>
+			  			
 			  		<?php } else { ?>				  		
-			  			<a href="<?php echo base_url('permisos/desactivar/'.$value->id) ;?>">Desactivar</a>
+			  			<a href="<?php echo base_url('permisos/desactivar/'.$value->id) ;?>"><input type="checkbox" checked></a>
+			  			
 			  		<?php }	
 			  		echo "</td>";
 			  		echo "</tr>";
