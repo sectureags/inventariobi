a6bdd9eeddd416bae53082087759cc757f45dd19
noraescareno <nescareno93@gmail.com>
2015-07-29 23:53:04 -0500

    Issue #38 Cambio en Seccion de CPU del Empleado

--- application/models/tbl_cpu_crud_model.php
+++ application/models/tbl_cpu_crud_model.php
@@ -20,11 +20,12 @@ class Tbl_cpu_crud_model extends CI_Model
 		$this->db->from('tbl_cpu');
 		$this->db->join('tbl_empleados', 'tbl_cpu.id_empleado=tbl_empleados.id_empleado');
 		$this->db->where('tbl_cpu.id_empleado',$id_empleado);
+		$this->db->join('status_cpus','tbl_cpu.status=status_cpus.id');
 		$res=$this->db->get();
 		return $res->result(); 
 	}
 
-	public function agregar_cpu($num_inventario,$categoria,$marca,$modelo,$hostname,$num_serie,$tipo,$ubicacion,$status,$id_empleado)
+	public function agregar_cpu($num_inventario,$categoria,$marca,$modelo,$hostname,$num_serie,$tipo,$ubicacion,$id_empleado,$status)
 	{
 		$data=array('num_inventario' => $num_inventario,
 					'categoria' => $categoria,
@@ -34,15 +35,11 @@ class Tbl_cpu_crud_model extends CI_Model
 					'num_serie' => $num_serie,
 					'tipo' => $tipo, 
 					'ubicacion'=>$ubicacion, 
-					'status' => $status,
-					'id_empleado'=>$id_empleado);
+					'id_empleado'=>$id_empleado,
+					'status' => $status,);
 		$nuevo = $this->db->insert('tbl_cpu', $data);
 	}
 
-	public function cargar_status()
-	{
-		$res=$this->db->get('status_cpus');////cuando agrego get solo hago referencia al select
-		return $res->result();
-	}
+	
 } 
 ?>
